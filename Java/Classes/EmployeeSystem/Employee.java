ef286563008591bd3c6e9978991159e0b0a84e2c
Caleb Kenney <65873072+Joelkenn1@users.noreply.github.com>
2025-03-06 02:59:30 -0500

    Create Employee.java

---
+++ Java/Classes/EmployeeSystem/Employee.java
@@ -0,0 +1,23 @@
+package ex4;
+
+public abstract class Employee {
+
+	private String name;
+
+	
+	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	public abstract double CalcPay();
+		
+		//return -1.0
+	
+	
+}
