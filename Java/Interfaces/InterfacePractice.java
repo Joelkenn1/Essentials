a11b6749c63a8fcd4f36a06ee71a42e1cd660d2d
Caleb Kenney <65873072+Joelkenn1@users.noreply.github.com>
2025-03-06 02:41:17 -0500

    Create InterfacePractice.java

---
+++ Java/Interfaces/InterfacePractice.java
@@ -0,0 +1,10 @@
+//Must be compiled first
+public interface InterfacePractice {
+	
+ 		public static final int MAX = 5;
+		
+		public void getGradYear();
+		public void getName();
+		public void isStudent(); 
+		public void showInfo(); 
+}
