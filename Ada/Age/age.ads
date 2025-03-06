f7834755a0149024ce33d54f19b53ce255a4cea4
Caleb Kenney <65873072+Joelkenn1@users.noreply.github.com>
2025-03-06 03:23:19 -0500

    Add files via upload

---
+++ Ada/Age/age.ads
@@ -0,0 +1,5 @@
+with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Integer_Text_IO; use Ada.Integer_Text_IO;
+
+-- In Ada you have to declare functions in seperate "ads" files
+function age (i : Integer) return string;
